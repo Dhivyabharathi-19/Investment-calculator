72aa516e102412c444e3d12fb0bd32332803278e
Dhivyabharathi-19 <dhivyabharathi.ca98@gmail.com>
2025-04-14 14:40:03 +0530

    using react js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Result from "./components/Result";
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsvalid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevuserInput) => {
+      return {
+        ...prevuserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {!inputIsvalid && (
+        <p className="center">Please enter a duration greater than Zero.</p>
+      )}
+      {inputIsvalid && <Result input={userInput} />}
+    </>
   );
 }
 
